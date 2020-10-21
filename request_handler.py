0e15785649546879d89615ec121fffd1858f776a
Gavin Schriver <alexanderschriver@gmail.com>
2020-10-21 15:52:30 -0500

    basic shell of request handler

---
+++ request_handler.py
@@ -0,0 +1,71 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
+
+class HandleRequests(BaseHTTPRequestHandler):
+    def parse_url(self, path):
+        path_params = path.split("/")
+        resource = path_params[1]
+        id = None
+
+        try:
+            id = int(path_params[2])
+        except IndexError:
+            pass 
+        except ValueError:
+            pass  
+
+        return (resource, id)  
+
+
+    def _set_headers(self, status):
+        self.send_response(status)
+        self.send_header('Content-type', 'application/json')
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.end_headers()
+
+    def do_GET(self):
+        self._set_headers(200)
+        response = {}  
+
+        (resource, id) = self.parse_url(self.path)
+
+        self.wfile.write(response.encode())
+
+    def do_POST(self):
+        self._set_headers(201)
+        content_len = int(self.headers.get('content-length', 0))
+        post_body = self.rfile.read(content_len)
+
+        post_body = json.loads(post_body)
+
+        (resource, id) = self.parse_url(self.path)
+
+        self.wfile.write(f"".encode())
+
+    def do_DELETE(self):
+        self._set_headers(204)
+
+        (resource, id) = self.parse_url(self.path)
+
+        self.wfile.write("".encode())
+
+    def do_PUT(self):
+        self._set_headers(204)
+        content_len = int(self.headers.get('content-length', 0))
+        post_body = self.rfile.read(content_len)
+        post_body = json.loads(post_body)
+
+
+        (resource, id) = self.parse_url(self.path)
+
+        self.wfile.write("".encode())
+
+
+def main():
+    host = ''
+    port = 8088
+    HTTPServer((host, port), HandleRequests).serve_forever()
+
+
+if __name__ == "__main__":
+    main()
