e2cb8f3d181c063f312e617522de5d8eb003fcea
mgjeffries <gib.jeffries@gmail.com>
2020-10-21 17:00:46 -0500

    format table creation to set up foreign keys

--- rare_server.session.sql
+++ rare_server.session.sql
@@ -7,19 +7,6 @@ CREATE TABLE `user` (
   `password` TEXT
 );
 
-CREATE TABLE `post` (
-  `id` INTEGER PRIMARY KEY AUTOINCREMENT,
-  `user_id` INTEGER,
-  `category_id` INTEGER,
-  `title` TEXT,
-  `content` TEXT,
-  `publication_time` INTEGER,
-  `creation_time` INTEGER,
-  `image` TEXT,
-  `publish_status` boolean,
-  `approve_status` boolean
-);
-
 CREATE TABLE `category` (
   `id` INTEGER PRIMARY KEY AUTOINCREMENT,
   `name` TEXT
@@ -33,16 +20,26 @@ CREATE TABLE `tag` (
 CREATE TABLE `post_tag` (
   `id` INTEGER PRIMARY KEY AUTOINCREMENT,
   `post_id` INTEGER,
-  `tag_id` INTEGER
+  `tag_id` INTEGER,
+  FOREIGN KEY (`post_id`) REFERENCES `post` (`id`),
+  FOREIGN KEY (`tag_id`) REFERENCES `tag` (`id`)
 );
 
-ALTER TABLE `post` ADD FOREIGN KEY (`category_id`) REFERENCES `category` (`id`);
+CREATE TABLE `post` (
+  `id` INTEGER PRIMARY KEY AUTOINCREMENT,
+  `user_id` INTEGER,
+  `category_id` INTEGER,
+  `title` TEXT,
+  `content` TEXT,
+  `publication_time` INTEGER,
+  `creation_time` INTEGER,
+  `image` TEXT,
+  `publish_status` boolean,
+  `approve_status` boolean,
+  FOREIGN KEY (`category_id`) REFERENCES `category` (`id`),
+  FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+);
 
-ALTER TABLE `post` ADD FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
-
-ALTER TABLE `post_tag` ADD FOREIGN KEY (`post_id`) REFERENCES `post` (`id`);
-
-ALTER TABLE `post_tag` ADD FOREIGN KEY (`tag_id`) REFERENCES `tag` (`id`);
 
 INSERT INTO 'category' VALUES (null, 'Buisness');
 INSERT INTO 'category' VALUES (null, 'Sports');
@@ -61,3 +58,10 @@ SELECT * FROM 'post';
 SELECT * FROM 'post_tag';
 SELECT * FROM 'tag';
 SELECT * FROM 'user';
+
+
+-- DROP TABLE 'category';
+-- DROP TABLE 'post';
+-- DROP TABLE 'post_tag';
+-- DROP TABLE 'tag';
+-- DROP TABLE 'user';
