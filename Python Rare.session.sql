85923ecdbcc376d5f9a62fd186773c088d123a54
Gavin Schriver <alexanderschriver@gmail.com>
2020-10-21 21:58:27 -0500

    switching to new branch for actual implementaiton of methods

---
+++ Python Rare.session.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `Category` (
+    `id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    `name` TEXT NOT NULL
+);
+
+INSERT INTO `Category` VALUES (null, "Tech");
+INSERT INTO `Category` VALUES (null, "British Politics");
+INSERT INTO `Category` VALUES (null, "Fuzzy Animals");
+INSERT INTO `Category` VALUES (null, "Random Nonsense");
